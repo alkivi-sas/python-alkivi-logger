e96d5a987a5c8789c51d4fa7541709e05b1f51e1
Anthony Martinet <anthony@alkivi.fr>
2017-04-05 12:48:33 +0200

    fix(build): when no pypandoc

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ try:
     import pypandoc
     long_description = pypandoc.convert(readme, 'rst')
 except(IOError, ImportError):
-    long_description = open(readme).read()
+    long_description = ""
 
 setup(
     name='alkivi-logger',
@@ -24,7 +24,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.2.2',
+    version='1.2.3',
 
     description='Python logger used at Alkivi',
     long_description=long_description,
